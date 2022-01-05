a76727b9b031273259bb36f474abc4eaa93bea95
trevin-livele <liveletrevin6@gmail.com>
2022-01-06 02:08:43 +0300

    new component forms

---
+++ src/app/forms/forms.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-forms',
+  templateUrl: './forms.component.html',
+  styleUrls: ['./forms.component.css']
+})
+export class FormsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+onSubmit(){
+  console.log(this.signupForm)
+}
+}
