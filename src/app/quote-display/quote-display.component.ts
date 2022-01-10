ad7da7d2c9072c13e904e81164a3e0d0950a7f63
trevin-livele <liveletrevin6@gmail.com>
2022-01-10 07:53:52 +0300

    removed comments

--- src/app/quote-display/quote-display.component.ts
+++ src/app/quote-display/quote-display.component.ts
@@ -7,24 +7,11 @@ import { Quotes } from '../quotes';
 })
 export class QuoteDisplayComponent implements OnInit {
 
-  // quotes: Quotes[] = [
-  //   new Quotes('Trevin', "trevin@gmail.com", "Love is not blind", 0, 0),
-  //   new Quotes('Livele', "livele@gmail.com", "Arsenal is the best team according to trevo", 0, 0),
-  //   new Quotes('Tracy', "tracy@gmail.com", "work hard", 0, 0),
-  // ]
+ 
   @Input() quotes: any
   constructor() { }
 
   ngOnInit(): void {
   }
-  // addUpvote(index: number){
-  //   // console.log(index)
-  //   console.log(this.quotes[index].upvote += 1)
-  // }
 
-
-  // removeUpvote(index: number){
-  //   // console.log(index)
-  //   console.log(this.quotes[index].downvote += 1)
-  // }
 }
